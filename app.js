3ace73cb79bf9421b326d2a5f0ffabba2de9536c
momoLearnsToRock <haratiasl@gmail.com>
2018-03-27 16:33:24 +0200

    working connection to Mongo and working get method

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var express = require('express');
+var mongoose=require('mongoose');
+var Book=require('./models/bookModel')
 
 var monApp=express();
+var db=mongoose.connect('mongodb:localhost/bookedAPI')
+// .then(
+//     function(){console.log('connected to mongo successfully');},
+//     function(err){console.log('failed to connect to mongo');}
+// );
 
+process.on('unhandledRejection', error => {
+    console.log('unhandledRejection', error);
+  });
+  
 var booksRouter=express.Router(); //looks at the actual objects it is not monApp.Router
 booksRouter.route('/Books') 
     .get(function(req,res){
-        res.send('Get a random book');
+        //res.send('Get a random book');
+        Book.find(function(err,books){
+            debugger;
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        })
     })
     .post(function (req, res) {
         res.send('Add a book');
+    })
+    .put(function (req, res) {
+        res.send('Update a book');
     });
 monApp.use('/API',booksRouter);
 
